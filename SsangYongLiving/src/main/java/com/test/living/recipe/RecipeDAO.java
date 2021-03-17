23a452dd82128cefae430b771c76ad4a0cf18600
cjdrkd7668 <74481989+cjdrkd7668@users.noreply.github.com>
2021-03-17 17:05:11 +0900

    레시피 게시판 기능 구현(+페이지네이션 class import)
    레시피 게시판을 구현했습니다.

---
+++ SsangYongLiving/src/main/java/com/test/living/recipe/RecipeDAO.java
@@ -0,0 +1,28 @@
+package com.test.living.recipe;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RecipeDAO implements IRecipeDAO {
+	
+	@Autowired
+	private SqlSessionTemplate template;
+
+	@Override
+	public List<RecipeDTO> getRecipeList(HashMap<String, Integer> map) {
+		
+		return template.selectList("recipe.getRecipeList", map);
+	}
+
+	@Override
+	public int getRecipeTotalCount() {
+		
+		return template.selectOne("recipe.getRecipeTotalCount");
+	}
+	
+}
